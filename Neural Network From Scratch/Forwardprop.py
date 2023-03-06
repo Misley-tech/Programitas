44ca0a157a4ebfa6a0b7d134b9bb081e11b9afd8
Misley-tech <77426773+Misley-tech@users.noreply.github.com>
2023-03-06 19:27:50 -0300

    Add files via upload

--- Neural Network From Scratch/Forwardprop.py
+++ Neural Network From Scratch/Forwardprop.py
@@ -2,49 +2,54 @@ import numpy as np
 class MLP:
     def __init__(self,num_imputs=3,num_hidden=[3, 3],num_outputs=2):
 
-        self.num_inputs = num_imputs # creo el atributo num_inputs 
-        self.num_hidden = num_hidden # creo el atributo num_hidden
-        self.num_outputs = num_outputs # creo el atributo num_outputs
+        self.num_inputs = num_imputs # num_inputs atribute is created
+        self.num_hidden = num_hidden # num_hidden atribute is created
+        self.num_outputs = num_outputs # num_outputs atribute is created
 
-        layers = [self.num_inputs] + self.num_hidden + [self.num_outputs]
+        # list that summarizes the number of nodes on each layer
+        layers = [self.num_inputs] + self.num_hidden + [self.num_outputs] 
         
-        #create random weights
-        weights = [] #creo una lista vacia
+        weights = [] #create an empty list
         for i in range(len(layers)-1):
-            w = np.random.rand(layers[i],layers[i+1])
-            self.weights.append(w)
-        self.weigths = weights
+            w = np.random.rand(layers[i],layers[i+1]) # creates random matrices of weights
+            self.weights.append(w) #appends those matrices to weights variable
+        self.weigths = weights #creates weights atribute  
 
     def forward_propagate(self, inputs):
         """ 
-        Metodo que calcula la forward propagation 
+        Method that calculate the forward propagation
+            args:
+                inputs (ndarray): input layer values (x1,x2,...,xn)
         """
         activations = inputs
         self.activations[0] = inputs
 
         for i,w in enumerate(self.weights):
-            #calculate net inputs
+            #calculate multiplication between input ndarray and the corresponding weight matrix
             net_inputs=np.dot(activations, w)
 
-            #calculate the activations
+            #applies the activation function over the weighted input (h=xi*Wi)
             activations = self._sigmoid(net_inputs)
             self.activations[i+1] = activations
-
         return activations
 
     def _sigmoid(self,x):
+        """ Method that implements the sigmoid function
+            args:
+                x (ndarray): input array (see Neural_network_from_scratch.pdf for further details)
+        """
         return 1/(1+np.exp(-x))
 
 if __name__ == "__main__":
-    #creo el Multi Layer Perceptron (MLP) y defino la forma que tiene la red neural
+    #call the Multi Layer Percerptron (MLP) class
     mlp = MLP()
 
-    #creo los inputs
+    #create an input with random values
     inputs = np.random.rand(mlp.num_inputs)
 
-    #ejecuto la forward propagation
+    #execute the forward propagation.
     outputs = mlp.forward_propagate(inputs)
 
-    #imprimo los resultados
+    #print results
     print(f"The network input is: {inputs}")
     print(f"The network output is: {outputs}")
